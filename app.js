d36f3ab043fd392f446fa4f4967dbc172f12149d
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-07-25 14:06:03 -0400

    set view engine to ejs, use public folder for styling

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/results', function(req, res) {
   res.send('Hello, it works!');
 });
